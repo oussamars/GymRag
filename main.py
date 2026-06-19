327da9992c6b189a7810204624d30c46547284cb
Oussama Bousselham <oussamabousselham2020@gmail.com>
2026-06-19 15:50:50 +0100

    chat that remembers history of previous prompts

--- main.py
+++ main.py
@@ -2,33 +2,44 @@ from google import genai
 import os
 from dotenv import load_dotenv
 from google.genai import types
-# from PIL import Image
-
-def main():
-    response = ask_gemini(
-        system_instruction = "You are a helpful gym assistant that gives fitness advice.",
-        # This fixed persona is important for RAG/chatbots because it ensures the AI stays consistent and focused on fitness advice regardless of user input.
-        content = "give me 5 push exercises i can do for this chest, shoulders and triceps workout"
-        )
-    print(response)
 
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-
-def ask_gemini(system_instruction, content):
-
+def send_message(history, text):
+    history.append(
+        types.Content(
+            role="user",
+            parts=[
+                types.Part(text=text)
+            ]
+        )
+    )
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         config=types.GenerateContentConfig(
-            system_instruction=system_instruction),
-        contents=content
+        system_instruction="You're my new teacher, you answer straight, strict to the point, using few words"),
+        contents=history
     )
 
-    return (response.text)
+    history.append(
+        types.Content(
+            role="model",
+            parts=[
+                types.Part(text=response.text)
+            ]
+        )
+    )
+    return(response.text)
 
-
+def main():
+    history = []
+    while(True):
+        prompt = input("You: ")
+        if (prompt == "quit"):
+            break
+        print(f"Gemini: {send_message(history, prompt)}")
 
 main()
